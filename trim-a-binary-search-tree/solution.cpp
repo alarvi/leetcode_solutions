b114d39fc8237526060a9bceb5ac850a1ecfaf02
Albert <alarvi650@gmail.com>
2020-01-05 12:05:05 +0100

    Add trim-a-binary-search-tree exercise solution

---
+++ trim-a-binary-search-tree/solution.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/trim-a-binary-search-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int L, int R) {
+        if (not root) return nullptr;
+        
+        if (root->val >= L and root->val <= R) {
+            root->left = trimBST(root->left, L, R);
+            root->right = trimBST(root->right, L, R);
+            return root;
+        }
+        
+        if (root->val < L) {
+            return trimBST(root->right, L, R);
+        }
+        
+        if (root->val > R) {
+            return trimBST(root->left, L, R);
+        }
+        
+        return nullptr;
+    }
+};
